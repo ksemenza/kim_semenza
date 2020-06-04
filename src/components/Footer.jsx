993fb2fc78d5746ca28b87a2fbb30c4b593f8f50
unknown <ksemenza13@gmail.com>
2020-06-04 18:12:06 -0400

    updated img

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,12 @@ import '../assests/css/Footer.css'
 const Footer = () => {
 
     return (
+
+        <div className='footer-wrap'>
         <div className='footer-ctr'>
             <p>   Developer:  Kim Semenza  <a href='#' className='footer-email'>(ksemenza.247@gmail.com)</a> ~ Guin Productions (c) 2020 </p>
       
+        </div>
         </div>
     )
 }
